e37414c6108e10244c99bbbc0ff0c8c526922d01
Michael Morris <mdam2345@gmail.com>
2018-06-22 03:56:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(cheddar_cheese)
-cheddar_cheese.include?.index { |cheese| cheese == "cheddar" }
+cheddar_cheese..index { |cheese| cheese == "cheddar" }
 end
