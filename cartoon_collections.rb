543750b7ce29a8088c14a3f8b624d2a4aea7690a
Michael Morris <mdam2345@gmail.com>
2018-06-22 03:11:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
 def roll_call_dwarves(dwarves)
-
+    dwarves.each do |dwarve,index|
+      new_array << array ** 2
+      end
+  new_array
+  end
 end
 
 def summon_captain_planet# code an argument here
