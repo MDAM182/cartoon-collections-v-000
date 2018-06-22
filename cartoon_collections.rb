8b63d15ae7ad74d743860bdc4d0c355d179da81e
Michael Morris <mdam2345@gmail.com>
2018-06-22 03:28:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(veggies)
     veggies.map do |calls|
-    calls.split(/ |\_/).map(&:capitalize).join(" ")
+    calls.split(/ |\_/)(&:capitalize).join(" ")
     end
 end
 
