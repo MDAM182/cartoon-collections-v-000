c726344004cf54f06b077d30a2ea3793072e7fd3
Michael Morris <mdam2345@gmail.com>
2018-06-22 03:50:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,14 +19,8 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(cheddar_cheese)
-  i = 0
-block_return_values = []
-while i < collection.length
-  block_return_values << yield(collection[i])
-  i = i + 1
-end
 
-if block_return_values.include?(false)
+if cheddar_cheese.include?(cheddar)
   false
 else
   true
